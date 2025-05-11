094497ee929b39a1b20e9033a5cfeaf4d618a2d3
MikailaAkeredolu <makerofappsdotcom@gmail.com>
2025-05-11 10:13:36 -0400

    ios tip calculator app demo for students

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,14 +8,91 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    //state for inputs
+    @State private var amount = 0.0
+    @State private var numberOfPeople = 1
+    @State private var tipPercentage = 18
+    
+    //Choice of tips
+    let tipChoices: [Int] = [10, 15, 18, 20, 25]
+    
+    // compute variable or computed property
+    //A computed property (or "computed variable") is a variable that doesn't store a value directly—instead, it calculates its value every time you access it.
+    
+    var totalPerPerson: Double {
+        
+        let peopleCount = numberOfPeople
+        print("peopleCount \(peopleCount) \n ===========")
+        
+        // Convert selected tip percentage from Int to Double for calculation and cast to a double
+        let tipSelection = Double(tipPercentage)
+        print("tipSelection \(tipSelection) \n ===========")
+        
+//        let tipValue = amount / 100 * tipSelection
+        let tipValue = tipSelection / 100
+        print("tipValue\(tipValue) \n ===========")
+        
+        let tipAmount = amount * tipValue
+        print("tip amount \(tipAmount) \n ===========")
+        
+        // Calculate the total amount including the tip
+        let grandTotal  = amount + tipAmount
+        print("grandTotal\(grandTotal) \n ===========")
+        
+        // Divide the total amount by number of people to get per-person cost by casting the int to a double
+        let amountPerPerson = grandTotal / Double(peopleCount)
+        print("amountPerPerson \(amountPerPerson) \n ===========")
+        return amountPerPerson
+        
+    }
+
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        
+        NavigationStack{
+            
+        
+        Form {
+            
+            Section(header: Text("Amount")){
+                TextField("Amount", value: $amount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                    .keyboardType(.decimalPad)
+                
+                Picker("Number of People", selection: $numberOfPeople) {
+                    //loop over and make each a text view
+                    ForEach(0..<100) { count in
+                        Text("\(count) people")
+                    }
+                }
+               // .pickerStyle(.navigationLink)
+             // .pickerStyle(SegmentedPickerStyle())
+                
+            }
+            
+            Section(header: Text("Tip Percentage")){
+                
+                Picker("Tip Percentage", selection: $tipPercentage) {
+                    ForEach(tipChoices, id: \.self) {
+                        Text($0, format: .percent)
+                    }
+                }
+                .pickerStyle(SegmentedPickerStyle())
+            }
+          
+            
+            Section(header: Text("Total Per Person")){
+                //display of input text
+                Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? " USD"))
+            }
+            
+        }.navigationTitle("WeSplit Calculator")
+         .navigationBarTitleDisplayMode(.inline)
+   
+        
+    }
+        
+        
     }
 }
 
--- WeSplit/WeSplitApp.swift
+++ WeSplit/WeSplitApp.swift
@@ -15,3 +15,65 @@ struct WeSplitApp: App {
         }
     }
 }
+
+
+
+/*
+ 
+ DEMO INTRO CODE
+ 
+ //property wrapper @State
+ @State private var tapCount = 0
+ @State private var name = ""
+ let numberOfBedrroms = ["1 bedroom", "2 bedrooms", "3 bedrooms"]
+ @State private var selectedNNumberOfBedrooms = "1 bedroom"
+ 
+ 
+ var body: some View {
+   
+     
+     NavigationStack{
+     
+     
+             Form{
+                 //2-way binding with $
+                 TextField("Enter the property address", text: $name)
+                 Text("Address is: \(name)")
+                 
+                 Section{
+                     ForEach(numberOfBedrroms, id: \.self){ name in
+                         Text(name)
+                     }
+                 }
+                 
+                 Section{
+                     Picker("no of bedrooms", selection: $selectedNNumberOfBedrooms){
+                         ForEach(numberOfBedrroms, id: \.self){
+                             Text($0)
+                         }
+                     }
+                 }
+            
+                 
+             }
+             //Add title to the form not the navStack
+             .navigationTitle("Swift UI")
+             .navigationBarTitleDisplayMode(.inline)
+         
+         
+     }
+     
+     Button("Tap Me"){
+          tapCount += 1     //action - increment count variable
+     }
+         .padding(20)
+         .foregroundStyle(Color.yellow)
+         .background(RoundedRectangle(cornerRadius: 20))
+     
+     Text("You tapped me \(tapCount) times")
+ 
+ 
+ 
+ 
+ 
+ */
